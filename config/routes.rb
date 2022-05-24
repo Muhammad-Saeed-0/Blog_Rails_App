9d55e83ab82085a94d38a3dd1f49d2383b633410
Muhammad-Saeed-0 <muhammad01111417159@gmail.com>
2022-05-24 20:34:39 +0200

    adding the posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  get 'test/index'
 
   root 'pages#home'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 10.times do |x|
+#   y = x + 1
+#   Post.create(title: "Title: #{y}", body: "Body: #{y} of title: #{y}")
+# end
